19e3ee592596cd6dca5bc08912c839102b34e8aa
Awungia112 <awungiaf@gmail.com>
2025-03-01 17:50:06 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::env;
 use get_pull_request::get_pr;
 use nacci::fibonacci;
-// use octocrab::Octocrab;
 use dotenv::dotenv;
 use post::post_comment;
-// use reqwest::Client;
 
 mod extract;
 mod nacci;
@@ -19,21 +17,11 @@ async fn main() {
     // Read environment variables
     let enable_fib = env::var("INPUT_ENABLE_FIB").unwrap_or_else(|_| "false".to_string());
     let max_threshold = env::var("INPUT_MAX_THRESHOLD").unwrap_or_else(|_| "100".to_string()).parse::<u64>().unwrap_or(100);
-    // let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN must be set");
-    // let repo_owner = env::var("GITHUB_REPOSITORY_OWNER").expect("GITHUB_REPOSITORY_OWNER must be set");
-    // let repo_name = env::var("GITHUB_REPOSITORY_NAME").expect("GITHUB_REPOSITORY_NAME must be set");
-    // let pr_number = env::var("GITHUB_PR_NUMBER").expect("GITHUB_PR_NUMBER must be set").parse::<u64>().unwrap();
-
+    
     println!("Enable Fib: {}", enable_fib);
     println!("Max Threshold: {}", max_threshold);
 
-    // // Initialize Octocrab with the GitHub token
-    // let octocrab = Octocrab::builder().personal_token(github_token).build().unwrap();
-    
-
-    // Get the pull request content
-    // let pr = octocrab.pulls(repo_owner.clone(), repo_name.clone()).get(pr_number).await.unwrap();
-    // // let pr_content = pr.body.as_deref().unwrap_or("");
+   
     let pr_number: u64 = env::var("PR_NUMBER")
     .expect("PR_NUMBER not set")
     .parse::<u64>()
@@ -51,28 +39,11 @@ async fn main() {
     };
     println!("values: {}", fibonacci_values);
 
-    // // Generate the Fibonacci sequence for a specified number of terms
-    // let num_terms = 45;
-    // let fibonacci_sequence = nacci::fibonacci_sequence(num_terms);
+    
 
     if let Err(e) = post_comment(&fibonacci_values).await {
         eprint!("Error posting comment: {}", e);
     }
     
 }
-// fn post_to_github(fib_numbers: &[i32]) -> Result<(), reqwest::Error> {
-//     let token = env::var("GITHUB_TOKEN")
-//         .unwrap_or_else(|_| panic!("Missing GITHUB_TOKEN environment variable"));
 
-//     let client = Client::new();
-//     let pr_url = env::var("GITHUB_API_URL")
-//         .expect("Missing GITHUB_API_URL environment variable")
-//         + "/repos/"
-//         + &env::var("GITHUB_REPOSITORY")
-//             .expect("Missing GITHUB_REPOSITORY environment variable")
-//         + "/issues/"
-//         + &env::var("GITHUB_REF")
-//             .expect("Missing GITHUB_REF environment variable")
-//         + "/comments";
-//     Ok(())
-// }
