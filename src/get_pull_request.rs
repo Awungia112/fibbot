ac3b11ce33b0bc8842de5a0a572c272566ae4944
Awungia112 <awungiaf@gmail.com>
2025-03-01 08:30:57 +0100

    added set-up files

--- src/get_pull_request.rs
+++ src/get_pull_request.rs
@@ -2,10 +2,9 @@ use crate::extract::extract;
 
 pub async fn get_pr(pr_number: u64) -> Vec<u32>{
 
-    let files = octocrab::instance().pulls("Nkwenti-severian-Ndongtsop", "Fibonacci-bot").list_files(pr_number).await;
+    let files = octocrab::instance().pulls("numsAwungia112", "fibbot").list_files(pr_number).await;
     let files = files.unwrap().items.first().unwrap().patch.clone().unwrap();
     println!("Pull Resquest Contents:\n{}",files);
     let nums = extract(&files.as_str().to_string());
-    println!("Collected Nums: {nums:?}");
     nums
  }
