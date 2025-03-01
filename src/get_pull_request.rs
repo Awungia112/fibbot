8c807db314183421087bd3cbda34cc151e5f7a40
Awungia112 <awungiaf@gmail.com>
2025-03-01 09:17:37 +0100

    added set-up files

--- src/get_pull_request.rs
+++ src/get_pull_request.rs
@@ -2,7 +2,7 @@ use crate::extract::extract;
 
 pub async fn get_pr(pr_number: u64) -> Vec<u32>{
 
-    let files = octocrab::instance().pulls("numsAwungia112", "fibbot").list_files(pr_number).await;
+    let files = octocrab::instance().pulls("Awungia112", "fibbot").list_files(pr_number).await;
     let files = files.unwrap().items.first().unwrap().patch.clone().unwrap();
     println!("Pull Resquest Contents:\n{}",files);
     let nums = extract(&files.as_str().to_string());
